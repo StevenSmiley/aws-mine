9c421dac4daed108acac8530048e1cd35e51ec82
Steven Smiley <smiley@scalesec.com>
2024-07-26 14:01:49 -0700

    add delete

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,11 @@ function App() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
 
+    
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
+
   return (
         
     <Authenticator>
@@ -28,7 +33,11 @@ function App() {
           <button onClick={createTodo}>+ new</button>
           <ul>
             {todos.map((todo) => (
-              <li key={todo.id}>{todo.content}</li>
+              <li           
+                onClick={() => deleteTodo(todo.id)}
+                key={todo.id}>
+                {todo.content}
+              </li>
             ))}
           </ul>
           <div>
