f224f732f43400b1be3d551e46d3dc056f871940
Steven Smiley <smiley@scalesec.com>
2024-07-26 17:19:56 -0700

    cloudscape

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import * as React from "react";
+// import * as React from "react";
 import { Authenticator } from '@aws-amplify/ui-react'
-// import '@aws-amplify/ui-react/styles.css'
+import '@aws-amplify/ui-react/styles.css'
 import "@cloudscape-design/global-styles/index.css"
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
@@ -11,12 +11,13 @@ import {
   BreadcrumbGroup,
   Container,
   ContentLayout,
-  Flashbar,
+  // Flashbar,
   Header,
   HelpPanel,
   Link,
   SideNavigation,
-  SplitPanel,
+  // SplitPanel,
+  TopNavigation,
 } from '@cloudscape-design/components';
 
 const client = generateClient<Schema>();
@@ -43,97 +44,109 @@ function App() {
     <Authenticator>
       {({ signOut }) => (
         <div>
-          <AppLayout
-              breadcrumbs={
-                <BreadcrumbGroup
-                  items={[
-                    { text: 'Home', href: '#' },
-                    { text: 'Service', href: '#' },
-                  ]}
-                />
-              }
-              navigationOpen={true}
-              navigation={
-                <SideNavigation
-                  header={{
-                    href: '#',
-                    text: 'aws-mine',
-                  }}
-                  items={[
-                    { type: "link", text: "Mines", href: "#/mines" },
-                    {
-                      type: "link",
-                      text: "Create a mine",
-                      href: "#/create"
-                    },
-                    { type: "link", text: "Other", href: "#/other" },
-                    { type: "divider" },
-                    {
-                      type: "link",
-                      text: "Notifications",
-                      href: "#/notifications",
-                      info: <Badge color="red">1</Badge>
-                    },
-                    {
-                      type: "link",
-                      text: "Documentation",
-                      href: "https://example.com",
-                      external: true
-                    }
-                  ]}
-                />
-              }
-              // notifications={
-              //   <Flashbar
-              //     items={[
-              //       {
-              //         type: 'info',
-              //         dismissible: true,
-              //         content: 'This is an info flash message.',
-              //         id: 'message_1',
-              //       },
-              //     ]}
-              //   />
-              // }
-              toolsOpen={true}
-              tools={<HelpPanel header={<h2>Help</h2>}>Help content</HelpPanel>}
-              content={
-                <ContentLayout
+          <div id="h" style={{ position: 'sticky', top: 0, zIndex: 1002 }}>
+          <TopNavigation
+          identity={{
+            href: '#',
+            title: 'aws-mine',
+          }}
+          utilities={[
+            {
+              type: 'button',
+              text: 'User Name',
+              iconName: 'user-profile',
+              // onClick allows user management
+            },
+            {
+              type: 'button',
+              text: 'Sign out',
+              onClick: () => signOut!(),
+            },
+          ]}
+        />
+        </div>
+        <AppLayout
+            breadcrumbs={
+              <BreadcrumbGroup
+                items={[
+                  { text: 'Home', href: '#' },
+                  { text: 'Service', href: '#' },
+                ]}
+              />
+            }
+            navigationOpen={true}
+            navigation={
+              <SideNavigation
+                items={[
+                  { type: "link", text: "Mines", href: "#/mines" },
+                  {
+                    type: "link",
+                    text: "Create a mine",
+                    href: "#/create"
+                  },
+                  { type: "link", text: "Other", href: "#/other" },
+                  { type: "divider" },
+                  {
+                    type: "link",
+                    text: "Notifications",
+                    href: "#/notifications",
+                    info: <Badge color="red">1</Badge>
+                  },
+                  {
+                    type: "link",
+                    text: "Documentation",
+                    href: "https://example.com",
+                    external: true
+                  }
+                ]}
+              />
+            }
+            // notifications={
+            //   <Flashbar
+            //     items={[
+            //       {
+            //         type: 'info',
+            //         dismissible: true,
+            //         content: 'This is an info flash message.',
+            //         id: 'message_1',
+            //       },
+            //     ]}
+            //   />
+            // }
+            toolsOpen={true}
+            tools={<HelpPanel header={<h2>Help</h2>}>Help content</HelpPanel>}
+            content={
+              <ContentLayout
+                header={
+                  <Header variant="h1" info={<Link variant="info">Info</Link>}>
+                    Mines
+                  </Header>
+                }
+              >
+                <Container
                   header={
-                    <Header variant="h1" info={<Link variant="info">Info</Link>}>
-                      Page header
+                    <Header variant="h2" description="Table content">
+                      Table of mines
                     </Header>
                   }
                 >
-                  <Container
-                    header={
-                      <Header variant="h2" description="Container description">
-                        Container header
-                      </Header>
-                    }
-                  >
-                    <div className="contentPlaceholder" />
-                  </Container>
-                </ContentLayout>
-              }
-            />
-          {/*
-          <button onClick={createMine}>+ new</button>
-          <ul>
-            {mines.map((mine) => (
-              <li           
-                onClick={() => deleteMine(mine.id)}
-                key={mine.id}>
-                {mine.description}
-                <br />
-                {mine.createdAt}
-              </li>
-            ))}
-          </ul>
-          <div>
-            <br />
-            <button onClick={signOut}>Sign out</button>
-          </div> */}
+                  // TODO: Populate a table
+                  <button onClick={createMine}>+ new</button>
+                  <ul>
+                    {mines.map((mine) => (
+                      <li           
+                        onClick={() => deleteMine(mine.id)}
+                        key={mine.id}>
+                        {mine.description}
+                        <br />
+                        {mine.createdAt}
+                      </li>
+                    ))}
+                  </ul>
+                </Container>
+              </ContentLayout>
+            }
+          />
         </div>
       )}
       </Authenticator>
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 
