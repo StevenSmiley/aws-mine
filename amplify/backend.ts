e336d891c9377d8f42256b3e2eeb8631fae04d7b
Steven Smiley <smiley@scalesec.com>
2024-07-27 20:28:37 -0700

    generate quarantined access key

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,4 +1,5 @@
 import { defineBackend } from '@aws-amplify/backend';
+import * as iam from "aws-cdk-lib/aws-iam"
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { generateMine } from './functions/generate-mine/resource';
@@ -15,3 +16,34 @@ cfnUserPool.adminCreateUserConfig = {
   allowAdminCreateUserOnly: true,
 };
 
+const customResourceStack = backend.createStack('AwsMineCustomResources');
+
+// Give the generateMine function permission to create IAM users
+const createQuarantinedUserStatement = new iam.PolicyStatement({
+  sid: "CreateUser",
+  actions: ["iam:CreateUser"],
+  conditions: [{
+      "StringLike": {
+        "iam:PermissionsBoundary": "arn:aws:iam::aws:policy/AWSCompromisedKeyQuarantineV2"
+      }
+    },
+    {
+      "StringEquals": {
+        "aws:RequestTag/aws-mine": "quarantined"
+      },
+    }],
+})
+// Give the generateMine function permission to create access keys for quarantied users only
+const createQuarantinedAccessKeysStatement = new iam.PolicyStatement({
+  sid: "CreateUser",
+  actions: ["iam:CreateAccessKeys"],
+  conditions: [{
+    "StringEquals": {
+      "aws:ResourceTag/aws-mine": "quarantined"
+    }
+  }],
+})
+
+const generateMineLambda = backend.generateMine.resources.lambda
+generateMineLambda.addToRolePolicy(createQuarantinedUserStatement)
+generateMineLambda.addToRolePolicy(createQuarantinedAccessKeysStatement)
--- amplify/functions/generate-mine/handler.ts
+++ amplify/functions/generate-mine/handler.ts
@@ -1,10 +1,40 @@
 import type { Schema } from "../../data/resource"
+import { IAMClient, CreateUserCommand, CreateAccessKeyCommand } from "@aws-sdk/client-iam";
+
+const iamClient = new IAMClient({});
+
+async function createIAMUserAndAccessKeys(username: string) {
+  try {
+    // Create IAM user
+    await iamClient.send(new CreateUserCommand({
+      UserName: username,
+      PermissionsBoundary: 'arn:aws:iam::aws:policy/AWSCompromisedKeyQuarantineV2',
+      Tags: [{ Key: 'aws-mine', Value: 'quarantined' }]
+    }
+    ));
+
+    // Create access keys for the user
+    const createAccessKeyParams = { UserName: username };
+    const accessKeyData = await iamClient.send(new CreateAccessKeyCommand(createAccessKeyParams));
+
+    return {
+      accessKeyId: accessKeyData.AccessKey?.AccessKeyId,
+      secretAccessKey: accessKeyData.AccessKey?.SecretAccessKey,
+    };
+  } catch (error) {
+    console.error("Error creating IAM user and access keys:", error);
+    throw error;
+  }
+}
 
 export const handler: Schema["GenerateMine"]["functionHandler"] = async (event) => {
-  const response = {
-    "accessKeyId": "akia1234",
-    "secretAccessKey": "secret1234",
+  try {
+    const username = `devops-admin-${crypto.randomUUID()}`;
+    const response = await createIAMUserAndAccessKeys(username);
+    console.log(response);
+    return response;
+  } catch (error) {
+    console.error("Error in handler:", error);
+    throw error;
   }
-  console.log(response)
-  return response;
 };
