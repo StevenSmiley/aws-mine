b5c9f73fa1a09bbd78f08d217bcc98fcce903328
Steven Smiley <smiley@scalesec.com>
2024-07-28 20:01:22 -0700

    refactor: removed debug logs

--- amplify/functions/tripped-mine/handler.ts
+++ amplify/functions/tripped-mine/handler.ts
@@ -4,19 +4,13 @@ import { promisify } from 'util';
 const gunzipAsync = promisify(gunzip);
 
 export const handler = async (event: any) => {
-  console.log("Event: ", event);
-  
   try {
     const payload = Buffer.from(event.awslogs.data, 'base64');
     const decompressed = await gunzipAsync(payload);
     const result = JSON.parse(decompressed.toString());
-    
-    console.log("Event Data:", JSON.stringify(result, null, 2));
 
     result.logEvents.forEach((logEvent: { message: string }) => {
       const message = JSON.parse(logEvent.message);
-      console.log("Parsed message:", message);
-      
       const interestingFields = {
         accessKeyId: message.userIdentity.accessKeyId,
         eventName: message.eventName,
@@ -25,7 +19,6 @@ export const handler = async (event: any) => {
         userAgent: message.userAgent,
         sourceIPAddress: message.sourceIPAddress,
       };
-      
       console.log("Interesting fields:", interestingFields);
     });
   } catch (error) {
