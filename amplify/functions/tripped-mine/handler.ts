c40ddc64d4a93ec13bba5505e1b4fbb1173b65c2
Steven Smiley <smiley@scalesec.com>
2024-07-28 19:43:17 -0700

    fix: decode and unzip event payload

--- amplify/functions/tripped-mine/handler.ts
+++ amplify/functions/tripped-mine/handler.ts
@@ -1,17 +1,28 @@
-export const handler = async (event: any) => {
+var zlib = require('zlib');
+
+export const handler = async (event: any, context: any) => {
   console.log("Event: ", event);
-  event.logEvents.forEach((logEvent: { message: string; }) => {
-    const message=JSON.parse(logEvent.message);
-    console.log("Parsed message:", message);
-    const interestingFields = {
-        accessKeyId: message.userIdentity.accessKeyId,
-        eventName: message.eventName,
-        eventSource: message.eventSource,
-        awsRegion: message.awsRegion,
-        userAgent: message.userAgent,
-        sourceIPAddress: message.sourceIPAddress,
+  var payload = Buffer.from(event.awslogs.data, 'base64');
+  zlib.gunzip(payload, function(e: any, result: any) {
+      if (e) { 
+          context.fail(e);
+      } else {
+          result = JSON.parse(result.toString());
+          console.log("Event Data:", JSON.stringify(result, null, 2));
       }
-      console.log("Interesting fields:", interestingFields)
+      result.logEvents.forEach((logEvent: { message: string; }) => {
+        const message=JSON.parse(logEvent.message);
+        console.log("Parsed message:", message);
+        const interestingFields = {
+            accessKeyId: message.userIdentity.accessKeyId,
+            eventName: message.eventName,
+            eventSource: message.eventSource,
+            awsRegion: message.awsRegion,
+            userAgent: message.userAgent,
+            sourceIPAddress: message.sourceIPAddress,
+          }
+          console.log("Interesting fields:", interestingFields)
+      });
   });
   return;
 };
