c23932e003050302e6a199d0012282517d494625
Steven Smiley <smiley@scalesec.com>
2024-07-28 19:20:19 -0700

    feat: parse fields from log event

--- amplify/functions/tripped-mine/handler.ts
+++ amplify/functions/tripped-mine/handler.ts
@@ -1,4 +1,17 @@
 export const handler = async (event: any) => {
-  console.log("Tripped mine");
+  console.log("Event: ", event);
+  event.logEvents.forEach((logEvent: { message: string; }) => {
+    const message=JSON.parse(logEvent.message);
+    console.log("Parsed message:", message);
+    const interestingFields = {
+        accessKeyId: message.userIdentity.accessKeyId,
+        eventName: message.eventName,
+        eventSource: message.eventSource,
+        awsRegion: message.awsRegion,
+        userAgent: message.userAgent,
+        sourceIPAddress: message.sourceIPAddress,
+      }
+      console.log("Interesting fields:", interestingFields)
+  });
   return;
 };
