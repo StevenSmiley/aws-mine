ab36170cd2aa64745a55916a049a7653f1d834c1
Steven Smiley <smiley@scalesec.com>
2024-07-28 19:48:29 -0700

    refactor: cleanup tripped mine function

--- amplify/functions/tripped-mine/handler.ts
+++ amplify/functions/tripped-mine/handler.ts
@@ -1,28 +1,34 @@
-var zlib = require('zlib');
+import { gunzip } from 'zlib';
+import { promisify } from 'util';
 
-export const handler = async (event: any, context: any) => {
+const gunzipAsync = promisify(gunzip);
+
+export const handler = async (event: any) => {
   console.log("Event: ", event);
-  var payload = Buffer.from(event.awslogs.data, 'base64');
-  zlib.gunzip(payload, function(e: any, result: any) {
-      if (e) { 
-          context.fail(e);
-      } else {
-          result = JSON.parse(result.toString());
-          console.log("Event Data:", JSON.stringify(result, null, 2));
-      }
-      result.logEvents.forEach((logEvent: { message: string; }) => {
-        const message=JSON.parse(logEvent.message);
-        console.log("Parsed message:", message);
-        const interestingFields = {
-            accessKeyId: message.userIdentity.accessKeyId,
-            eventName: message.eventName,
-            eventSource: message.eventSource,
-            awsRegion: message.awsRegion,
-            userAgent: message.userAgent,
-            sourceIPAddress: message.sourceIPAddress,
-          }
-          console.log("Interesting fields:", interestingFields)
-      });
-  });
-  return;
+  
+  try {
+    const payload = Buffer.from(event.awslogs.data, 'base64');
+    const decompressed = await gunzipAsync(payload);
+    const result = JSON.parse(decompressed.toString());
+    
+    console.log("Event Data:", JSON.stringify(result, null, 2));
+
+    result.logEvents.forEach((logEvent: { message: string }) => {
+      const message = JSON.parse(logEvent.message);
+      console.log("Parsed message:", message);
+      
+      const interestingFields = {
+        accessKeyId: message.userIdentity.accessKeyId,
+        eventName: message.eventName,
+        eventSource: message.eventSource,
+        awsRegion: message.awsRegion,
+        userAgent: message.userAgent,
+        sourceIPAddress: message.sourceIPAddress,
+      };
+      
+      console.log("Interesting fields:", interestingFields);
+    });
+  } catch (error) {
+    console.error("Error processing event:", error);
+  }
 };
